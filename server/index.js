4082d70e5c652459b207e937baa2358bc03283d5
C19rk <clarkmanuntag@gmail.com>
2026-01-14 15:57:34 +0800

    Fix for client domain

--- server/index.js
+++ server/index.js
@@ -13,6 +13,7 @@ import { checkLeaves } from "./src/cron/leaveChecker.js";
 import { cleanSched } from "./src/cron/cleanExpiredSched.js";
 import { bucket } from "./src/config/firebase.js";
 import pool from "./src/db.js";
+import { credential } from "firebase-admin";
 
 dotenv.config();
 
@@ -23,9 +24,22 @@ const port = process.env.PORT || 5001;
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
+// CORS config
+const corsOptions = {
+  origin: [
+    "http://localhost:5000",
+    "https://its-atttendance-system-yeur.vercel.app",
+    /\.vercel\.app$/, // allow all vercel domains
+  ],
+  credentials: true,
+  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH"],
+  allowedHeaders: ["Content-Type", "Authorization", "X-Requested-With"],
+  optionsSuccessStatus: 200,
+};
+
 // middlewares
-app.use(cors());
-app.options("*", cors());
+app.use(cors(corsOptions));
+app.options("*", cors(corsOptions));
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
